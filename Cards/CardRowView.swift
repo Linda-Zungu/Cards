051064f05788f174cd6f1a44354931d6ad7b1097
Linda Zungu <lindazungu@Lindas-MacBook-Pro.local>
2021-03-17 15:21:21 +0200

    Added the card number to the row and populated the card with card information

--- Cards/CardRowView.swift
+++ Cards/CardRowView.swift
@@ -10,15 +10,35 @@ import SwiftUI
 struct CardRowView: View {
     
     var cardName : String
+    var cardNumber : String
+    var expiryDate : String
     
     var body: some View {
         HStack{
             RoundedRectangle(cornerRadius: 5)
                 .frame(width: 70, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .overlay(
+                    VStack{
+                        Group{
+                            Text("\(cardNumber)")
+                            Text("\(expiryDate)")
+                        }
+                        .foregroundColor(.white)
+                        .font(.system(size: 5))
+                    }
+                    
+                )
                 .shadow(radius: 5, y: 8)
                 .foregroundColor(.blue)
                 .padding()
-            Text("\(cardName)")
+            
+            VStack(alignment: .leading){
+                Text("\(cardName)")
+                    .padding(.bottom, 1)
+                Text("\(cardNumber)")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
             Spacer()
         }
     }
@@ -26,7 +46,7 @@ struct CardRowView: View {
 
 struct CardRowView_Previews: PreviewProvider {
     static var previews: some View {
-        CardRowView(cardName: "Standard Bank")
+        CardRowView(cardName: "Standard Bank", cardNumber: "0001000200030004", expiryDate: "08/24")
             .previewLayout(.sizeThatFits)
     }
 }
