19568b80f00cd434070840ce611287ddd67a07ac
Linda Zungu <linda.zungu@icloud.com>
2021-03-20 23:48:12 +0200

    Seperate BlurView Card

---
+++ Cards/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  Cards
+//
+//  Created by Linda Zungu on 2021/03/20.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
