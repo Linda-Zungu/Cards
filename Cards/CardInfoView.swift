4341dea047b185162b96513216a1c13576980435
Linda Zungu <linda.zungu@icloud.com>
2021-03-24 15:29:40 +0200

    Added a map view

--- Cards/CardInfoView.swift
+++ Cards/CardInfoView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct CardInfoView: View {
     @Environment(\.colorScheme) var colorScheme
@@ -20,15 +21,21 @@ struct CardInfoView: View {
     @State var isTapped = true
 
     @State var isCardNotTapped = true
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -34.0999, longitude: 18.4241), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
     var body: some View {
         ScrollView{
             VStack{
+                Map(coordinateRegion: $region, interactionModes: [], showsUserLocation: true, userTrackingMode: .constant(.follow))
+                    .frame(width: UIScreen.main.bounds.width-40, height: 450, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .cornerRadius(15)
+                    .padding()
+                
                 CardView(isNotTapped: isCardNotTapped, isTapped: isTapped, cvvNumber: cvvNumber, cardNumber: cardNumber, expiryDate: expiryDate, selectedBank: selectedBank, cardHolder: cardHolder)
                     .onTapGesture {
                         isCardNotTapped.toggle()
                         isTapped = false
                     }
-                    
             }
         }
         .navigationBarTitle("\(selectedBank)")
