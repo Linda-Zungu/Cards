7578292ef4587d58e8c39d6a09e5d5dec090347a
Linda Zungu <linda.zungu@icloud.com>
2021-03-31 08:43:07 +0200

    Annotations show up when the map view shows up.

--- Cards/MapView.swift
+++ Cards/MapView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 import MapKit
 
 struct MapView: UIViewRepresentable {
+    var nameOfBank : String
+    
     let locationManager = CLLocationManager()
     
     func makeUIView(context: Context) -> MKMapView {
@@ -31,12 +33,51 @@ struct MapView: UIViewRepresentable {
             let span = MKCoordinateSpan(latitudeDelta: 0.009, longitudeDelta: 0.009)
             let region = MKCoordinateRegion(center: location, span: span)
             uiView.setRegion(region, animated: true)
+            
+            let searchRequest = MKLocalSearch.Request()
+            searchRequest.naturalLanguageQuery = nameOfBank
+//            searchRequest.region = uiView.regionThatFits(MKCoordinateRegion(center: location, span: MKCoordinateSpan(latitudeDelta: 1.00, longitudeDelta: 1.00)))
+            searchRequest.region.center = uiView.region.center
+            let search = MKLocalSearch(request: searchRequest)
+            search.start { response, error in
+                guard let response = response else{
+                    print("Error: \(error?.localizedDescription ?? "Unknown error").")
+                    return
+                }
+                
+                for item in response.mapItems{
+                    let place = MKPointAnnotation()
+                    place.title = searchRequest.naturalLanguageQuery
+                    place.coordinate = item.placemark.coordinate
+                    uiView.addAnnotation(place)
+                }
+            }
         }
+        
+//        let searchRequest = MKLocalSearch.Request()
+//        searchRequest.naturalLanguageQuery = "Standard Bank"
+//        searchRequest.region = uiView.regionThatFits(.init(center: locationManager.location?.coordinate ?? (CLLocationCoordinate2D(latitude: -34.0999, longitude: 18.4241)), span: MKCoordinateSpan(latitudeDelta: 0.009, longitudeDelta: 0.009)))
+//        let search = MKLocalSearch(request: searchRequest)
+//        search.start { response, error in
+//            guard let response = response else {
+//                print("Error: \(error?.localizedDescription ?? "Unknown error").")
+//                return
+//            }
+//
+//            for item in response.mapItems {
+//                print(item.placemark.coordinate)
+//                let london = MKPointAnnotation()
+//                london.title = searchRequest.naturalLanguageQuery
+//                london.coordinate = item.placemark.coordinate
+//                uiView.addAnnotation(london)
+//
+//            }
+//        }
     }
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(nameOfBank: "Standard Bank")
     }
 }
