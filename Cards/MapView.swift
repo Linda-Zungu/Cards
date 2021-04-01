5df3f38d2a49a3dbe9531f0579d0255b5aa26b7c
Linda Zungu <linda.zungu@icloud.com>
2021-04-01 05:07:29 +0200

    Removed some comments, and changed the span of the map

--- Cards/MapView.swift
+++ Cards/MapView.swift
@@ -30,14 +30,14 @@ struct MapView: UIViewRepresentable {
             locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
             locationManager.startUpdatingLocation()
             let location: CLLocationCoordinate2D = locationManager.location!.coordinate
-            let span = MKCoordinateSpan(latitudeDelta: 0.009, longitudeDelta: 0.009)
+            let span = MKCoordinateSpan(latitudeDelta: 0.059, longitudeDelta: 0.059)
             let region = MKCoordinateRegion(center: location, span: span)
             uiView.setRegion(region, animated: true)
             
             let searchRequest = MKLocalSearch.Request()
             searchRequest.naturalLanguageQuery = nameOfBank
-//            searchRequest.region = uiView.regionThatFits(MKCoordinateRegion(center: location, span: MKCoordinateSpan(latitudeDelta: 1.00, longitudeDelta: 1.00)))
-            searchRequest.region.center = uiView.region.center
+            searchRequest.region = uiView.regionThatFits(MKCoordinateRegion(center: location, span: MKCoordinateSpan(latitudeDelta: 1.00, longitudeDelta: 1.00)))
+//            searchRequest.region.center = uiView.region.center
             let search = MKLocalSearch(request: searchRequest)
             search.start { response, error in
                 guard let response = response else{
@@ -53,26 +53,6 @@ struct MapView: UIViewRepresentable {
                 }
             }
         }
-        
-//        let searchRequest = MKLocalSearch.Request()
-//        searchRequest.naturalLanguageQuery = "Standard Bank"
-//        searchRequest.region = uiView.regionThatFits(.init(center: locationManager.location?.coordinate ?? (CLLocationCoordinate2D(latitude: -34.0999, longitude: 18.4241)), span: MKCoordinateSpan(latitudeDelta: 0.009, longitudeDelta: 0.009)))
-//        let search = MKLocalSearch(request: searchRequest)
-//        search.start { response, error in
-//            guard let response = response else {
-//                print("Error: \(error?.localizedDescription ?? "Unknown error").")
-//                return
-//            }
-//
-//            for item in response.mapItems {
-//                print(item.placemark.coordinate)
-//                let london = MKPointAnnotation()
-//                london.title = searchRequest.naturalLanguageQuery
-//                london.coordinate = item.placemark.coordinate
-//                uiView.addAnnotation(london)
-//
-//            }
-//        }
     }
 }
 
