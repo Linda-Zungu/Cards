025b27c0254f10d126ab8add68fd7bb9c91f082c
Linda Zungu <linda.zungu@icloud.com>
2021-04-03 00:38:52 +0200

    Reworked colors in the cards

--- Cards/CardRowView.swift
+++ Cards/CardRowView.swift
@@ -18,6 +18,10 @@ struct CardRowView: View {
         HStack{
             RoundedRectangle(cornerRadius: 5)
                 .frame(width: 70, height: 45, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .background(
+                    getColorForBank(bankName: cardName)
+                        .cornerRadius(5)
+                )
                 .overlay(
                     VStack(alignment: .leading){
                         Group{
@@ -27,6 +31,7 @@ struct CardRowView: View {
                                     .padding(.top, 4)
                                     .padding(.bottom, 0.1)
                                     .padding(.leading, 5)
+                                    .foregroundColor(/*cardName == "Capitec Bank" ? Color.yellow.opacity(0.8) : */.white)
                                 
                                 Spacer()
                                 
@@ -43,10 +48,12 @@ struct CardRowView: View {
                             Text("\(cardNumber)")
                                 .tracking(0.5)
                                 .padding(.leading, cardNumber.count == 16 ? 5 : 11)
+                                .foregroundColor(/*cardName == "Capitec Bank" ? Color.yellow.opacity(0.8) : */.white)
                                 
                             Text("\(expiryDate)")
                                 .offset(x: 28, y: 2)
                                 .padding(.leading, 5)
+                                .foregroundColor(/*cardName == "Capitec Bank" ? Color.yellow.opacity(0.8) : */.white)
                         }
                         .foregroundColor(.white)
                         .font(.system(size: 5))
@@ -56,7 +63,7 @@ struct CardRowView: View {
                     
                 )
                 .shadow(radius: 5, y: 8)
-                .foregroundColor(.blue)
+                .foregroundColor(.clear)
                 .padding()
             
             VStack(alignment: .leading){
@@ -95,11 +102,35 @@ struct CardRowView: View {
             return "NoImage"
         }
     }
+    
+    private func getColorForBank(bankName : String) -> LinearGradient{
+        if(bankName == "Standard Bank"){
+            return LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.65), Color.init(.displayP3, red: 0, green: 0.25, blue: 0.8, opacity: 1)]), startPoint: .top, endPoint: .bottom)
+        }
+        else if(bankName == "Absa Bank"){
+            return LinearGradient(gradient: Gradient(colors: [Color.red.opacity(0.75), Color.init(.displayP3, red: 0.55, green: 0, blue: 0.37, opacity: 1)]), startPoint: .top, endPoint: .bottom)
+        }
+        else if(bankName == "African Bank"){
+            return LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.75), Color.init(.displayP3, red: 0, green: 0.3, blue: 0, opacity: 1), Color.init(.displayP3, red: 0, green: 0, blue: 0, opacity: 1)]), startPoint: .top, endPoint: .bottom)
+        }
+        else if(bankName == "Bidvest Bank"){
+            return LinearGradient(gradient: Gradient(colors: [Color.black.opacity(1), Color.init(.displayP3, red: 0, green: 0, blue: 0.52, opacity: 1)]), startPoint: .top, endPoint: .bottomLeading)
+        }
+        else if(bankName == "Capitec Bank"){
+            return LinearGradient(gradient: Gradient(colors: [Color.black.opacity(1), Color.black]), startPoint: .top, endPoint: .bottom)
+        }
+        else if(bankName == "Discovery"){
+            return LinearGradient(gradient: Gradient(colors: [Color.gray.opacity(0.5), Color.black]), startPoint: .top, endPoint: .bottomTrailing)
+        }
+        else{
+            return LinearGradient(gradient: /*@START_MENU_TOKEN@*/Gradient(colors: [Color.red, Color.blue])/*@END_MENU_TOKEN@*/, startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/)
+        }
+    }
 }
 
 struct CardRowView_Previews: PreviewProvider {
     static var previews: some View {
-        CardRowView(cardName: "Standard Bank", cardNumber: "0001000200030", expiryDate: "08/24", cardType: "MasterCard_Dark")
+        CardRowView(cardName: "Discovery", cardNumber: "0001000200030", expiryDate: "08/24", cardType: "MasterCard_Dark")
             .previewLayout(.sizeThatFits)
     }
 }
